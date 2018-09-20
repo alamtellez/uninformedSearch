6609e4c37bbdaa1300602a410e0dd79fe45a6566
FerminAMC <FerminAMC@github.com>
2018-09-19 23:46:39 -0500

    UCS and AStar with consistent and inconsistent heuristic

--- AStar.py
+++ AStar.py
@@ -1,5 +1,16 @@
+'''
+Input:
+2
+(A); (B); (C)
+(A, C); X; X
+Output:
+3
+(2, 0)
+'''
 import heapq
 from copy import deepcopy
+from random import random
+
 visited = set()
 
 # Node class definition
@@ -9,19 +20,20 @@ class Node():
         self.state = deepcopy(stacks)
         self.path_cost = 0
         self.heuristic_cost = 0
+        self.f_cost = 0
         self.action = None
         self.children = []
         self.stacksize = sz
         self.key = ""
     def __lt__(self, other):
-        return self.path_cost < other.path_cost
+        return self.f_cost < other.f_cost
 
     # This method recieves a node with its parent state and applies the action given
     def applyAction(self):
         move = self.action
         item = self.state[move[0]].pop()
         self.state[move[1]].append(item)
-        self.path_cost = self.parent.path_cost + 1 + abs(move[1] - move[0])
+        self.path_cost = self.parent.path_cost + 1 + abs(move[1] - move[0]) + self.heuristic_cost
 
     # Optional key for visited if needed
     def setKey(self):
@@ -29,8 +41,9 @@ class Node():
             for j in range(len(self.state[i])):
                 self.key += str(j)
             self.key += ";"
+
     # Function that expands the node to the possible children
-    def possibleChildren(self):
+    def possibleChildren(self, goal):
         for i in range(len(self.state)):
             for j in range(len(self.state)):
                 if len(self.state[i]) > 0:
@@ -40,7 +53,23 @@ class Node():
                         auxNode.parent = self
                         auxNode.applyAction()
                         auxNode.setKey()
+                        auxNode.calcHeuristicCost(goal)
                         self.children.append(auxNode)
+
+    # A consistent heuristic that increases whenever a container
+    # is not in the goal state
+    def calcHeuristicCost(self, goal):
+        for i in range(len(self.state)):
+            for j in range(min(len(goal.state[i]), len(self.state[i]))):
+                if(self.state[i][j] != goal.state[i][j]):
+                    self.heuristic_cost += 1
+        self.f_cost = self.heuristic_cost + self.path_cost
+
+    # This is an inconsistent heuristic that will most likely
+    # make A Star fail and not find an optimal solution
+    def calcInconsistentHeuristic(self):
+        self.heuristic_cost = random.randint(0, 100)
+        self.f_cost = self.heuristic_cost + self.path_cost
 
 # Parsing the input into:
 #   limit height of the problem
@@ -58,7 +87,7 @@ def checkValid(stacks,max_height):
         if len(stack) > max_height:
             return False
     return True
-# tkes a node and a list to apped the path recursively
+# takes a node and a list to apped the path recursively
 def print_action(node, actions):
     if (node.parent == None):
         return
@@ -91,25 +120,20 @@ if __name__ == "__main__":
     if not checkValid(initial.state, max_height):
         print('No solution found')
         exit()
-    # Add initial to frontier to start
+        
     heapq.heappush(min_heap, initial)
     while(True):
-        # If there are no elements in thequeue then there is n solution
         if(len(min_heap) == 0):
             print("No solution found")
             exit()
-        # Take out the first/smallest element to evaluate if it is our goal
         current = heapq.heappop(min_heap)
         visited.append(current)
-        # Expand the node
-        current.possibleChildren()
-        # Check if the node is our goal
+        current.possibleChildren(goalNode)
         if is_goal(current, goalNode):
-            print(current.path_cost)
+            print(current.f_cost)
             path = []
             print_action(current, path)
             print("; ".join(map(str, path)))
             break
-        # If node isnt goal, expand it nd continue searching
         for i in range(len(current.children)):
             heapq.heappush(min_heap, current.children[i])
